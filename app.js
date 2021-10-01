d6ccec3a53776ed72eaaa8ab2b2a122f8940fc1a
unknown <y.kharoufi96@gmail.com>
2021-10-01 04:04:33 -0700

    update2 for heroku v1.0.0

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.set('view engine', 'twig');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("secret"));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('body-parser').urlencoded({ extended: true }));
